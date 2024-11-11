2dd5a4a938170a4d374d592bf37bc2f330be499c
Vincent Lau <35178680+GalacticKnight@users.noreply.github.com>
2024-11-10 23:01:19 -0500

    first

---
+++ 1HelloWorld.java
@@ -0,0 +1,8 @@
+public class 1HelloWorld{
+    public String sayHello(){
+        return "Hello, world!";
+    }
+    public static void main(String[] args){
+        System.out.println(sayHello());
+    }
+}
