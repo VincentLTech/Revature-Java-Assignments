0b5fc997883b5e7e74c44d5364df3a66f79c8568
Vincent <vincent.lau.cs@gmail.com>
2025-01-17 01:57:36 -0500

    target

--- Assignments/Week8/Day1/AggregateFunctionsActivity.sql
+++ Assignments/Week8/Day1/AggregateFunctionsActivity.sql
@@ -1,3 +1,14 @@
+SELECT SUM(salary) AS "total_salary" FROM employee;
+
+SELECT COUNT(last_name) 
+FROM employee
+WHERE last_name='Smith';
+
+SELECT MIN(salary) FROM employee;
+
+SELECT MAX(salary) FROM employee;
+
+
 
 import Util.ConnectionUtil;
 import Util.FileUtil;
